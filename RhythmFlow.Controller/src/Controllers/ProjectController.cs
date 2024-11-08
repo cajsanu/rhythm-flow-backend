c4b35a78f7520f8b7fa3030e9233e855de701dbb
Cajsa Nummelin <cajsa.nummelin@gmail.com>
2024-11-08 21:53:08 +0200

    add not yet implemented methods to controllers

---
+++ RhythmFlow.Controller/src/Controllers/ProjectController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using RhythmFlow.Domain.src.Entities;
+
+namespace RhythmFlow.Controller.src.Controllers
+{
+    public class ProjcetController(IBaseService<Project> service) : BaseController<Project>(service)
+    {
+        // Get workspace by id should include all projects in the workspace 
+        // so no need to have a separate method for this.
+
+        [HttpPost("assignUser/{userId}")]
+        public async Task<ActionResult> AssignUserToProject(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("removeUser/{userId}")]
+        public async Task<ActionResult> RemoveUserFromProject(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
