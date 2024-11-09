2f0059adb22385afaf19c1e6a49af14233a50252
Cajsa Nummelin <cajsa.nummelin@gmail.com>
2024-11-09 16:30:26 +0200

    create rest of the service interfaces

---
+++ RhythmFlow.Application/src/Abstractions/IProjectService.cs
@@ -0,0 +1,9 @@
+using RhythmFlow.Domain.src.Entities;
+
+namespace RhythmFlow.Application.src.Abstractions
+{
+    public interface IProjectService : IBaseService<Project>
+    {
+        
+    }
+}
