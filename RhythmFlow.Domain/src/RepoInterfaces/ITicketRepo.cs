63e4eadcd39f2271d2d6c658a1331395ec83ea44
Cajsa Nummelin <cajsa.nummelin@gmail.com>
2024-11-09 11:25:13 +0200

    add comments

--- RhythmFlow.Domain/src/RepoInterfaces/ITicketRepo.cs
+++ RhythmFlow.Domain/src/RepoInterfaces/ITicketRepo.cs
@@ -4,5 +4,8 @@ namespace RhythmFlow.Domain.src.RepoInterfaces
 {
     public interface ITicketRepo : IBaseRepo<Ticket>
     {
+        // Add methods for assigning a ticket to a user and removing a ticket from a user
+        // AssignTicketToUser()
+        // RemoveTicketFromUser()
     }
 }
