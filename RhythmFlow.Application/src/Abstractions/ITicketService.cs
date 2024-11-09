ef3e6776a01036ac776bfac255f7b3b7abb43c93
Cajsa Nummelin <cajsa.nummelin@gmail.com>
2024-11-09 16:30:26 +0200

    create rest of the service interfaces

---
+++ RhythmFlow.Application/src/Abstractions/ITicketService.cs
@@ -0,0 +1,8 @@
+using RhythmFlow.Domain.src.Entities;
+
+namespace RhythmFlow.Application.src.Abstractions
+{
+    public interface ITicketService : IBaseService<Ticket>
+    {
+    }
+}
---
+++ RhythmFlow.Application/src/Abstractions/IWorkspaceService.cs
@@ -0,0 +1,9 @@
+using RhythmFlow.Domain.src.Entities;
+
+namespace RhythmFlow.Application.src.Abstractions
+{
+    public interface IWorkspaceService : IBaseService<Workspace>
+    {
+        
+    }
+}
