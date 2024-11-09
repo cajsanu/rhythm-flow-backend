5f82e764688baf56aecb028dbfbf9e1d928dbe6b
Cajsa Nummelin <cajsa.nummelin@gmail.com>
2024-11-09 17:09:45 +0200

    add methods to IWorkspaceService

--- RhythmFlow.Application/src/Abstractions/IWorkspaceService.cs
+++ RhythmFlow.Application/src/Abstractions/IWorkspaceService.cs
@@ -6,5 +6,11 @@ namespace RhythmFlow.Application.src.Abstractions
     {
         // Can we somehow extract the logic for adding/removing users
         // to/from a workspace/project/ticket to a separate service?
+
+        // Methdo to get all workspaces owned by a user'
+        Task <IEnumerable<Workspace>> GetAllWorkspaceOwnedByUser(Guid userId);
+
+        // Method to get all workspaces joined by a user
+        Task <IEnumerable<Workspace>> GetAllWorkspaceJoinedByUser(Guid userId);
     }
 }
