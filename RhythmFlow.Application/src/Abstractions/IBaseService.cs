25bd1b5145293a4749d80e6c0c777671aaf75535
Cajsa Nummelin <cajsa.nummelin@gmail.com>
2024-11-09 12:57:51 +0200

    resolve merge conflict

---
+++ RhythmFlow.Application/src/Abstractions/IBaseService.cs
@@ -0,0 +1,13 @@
+using RhythmFlow.Domain.src.Entities;
+
+namespace RhythmFlow.Application.src.Abstractions
+{
+    public interface IBaseService<T> where T : BaseEntity
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(Guid id);
+        Task<T> AddAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task DeleteAsync(Guid id);
+    }
+}
