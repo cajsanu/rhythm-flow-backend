99707475b715cc7730c0deea5fa9084f7a9d71c6
Cajsa Nummelin <cajsa.nummelin@gmail.com>
2024-11-09 16:44:15 +0200

    add comments

--- RhythmFlow.Application/src/Abstractions/IWorkspaceService.cs
+++ RhythmFlow.Application/src/Abstractions/IWorkspaceService.cs
@@ -4,6 +4,7 @@ namespace RhythmFlow.Application.src.Abstractions
 {
     public interface IWorkspaceService : IBaseService<Workspace>
     {
-        
+        // Can we somehow extract the logic for adding/removing users
+        // to/from a workspace/project/ticket to a separate service?
     }
 }
