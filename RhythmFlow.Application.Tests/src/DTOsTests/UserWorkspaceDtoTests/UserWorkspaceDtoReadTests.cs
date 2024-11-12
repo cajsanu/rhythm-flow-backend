94d91b470a0be9aaf39c1d1e86f602d9e9268b59
Anouar Belila <anouarbelila@Anouars-MacBook-Pro.local>
2024-11-12 14:48:38 +0100

    Pull from merge

--- RhythmFlow.Application.Tests/src/DTOsTests/UserWorkspaceDtoTests/UserWorkspaceDtoReadTests.cs
+++ RhythmFlow.Application.Tests/src/DTOsTests/UserWorkspaceDtoTests/UserWorkspaceDtoReadTests.cs
@@ -16,7 +16,7 @@ namespace RhythmFlow.Application.Tests.src.DTOsTests.UserWorkspaceCreateDtoTests
 
         [Theory]
         [InlineData("6fa85f64-5717-4562-b3fc-2c963f66afa6", "6fa85f64-5717-4562-b3fc-2c963f66afa6", RoleEnum.Developer, true)] // Valid case
-        [InlineData("00000000-0000-0000-0000-000000000000", "6fa85f64-5717-4562-b3fc-2c963f66afa6", RoleEnum.PojectManager, false)] // Invalid UserId (empty GUID)
+        [InlineData("00000000-0000-0000-0000-000000000000", "6fa85f64-5717-4562-b3fc-2c963f66afa6", RoleEnum., false)] // Invalid UserId (empty GUID)
         [InlineData("6fa85f64-5717-4562-b3fc-2c963f66afa6", "00000000-0000-0000-0000-000000000000", RoleEnum.PojectManager, false)] // Invalid WorkspaceId (empty GUID)
         [InlineData("6fa85f64-5717-4562-b3fc-2c963f66afa6", "6fa85f64-5717-4562-b3fc-2c963f66afa6", (RoleEnum)999, false)] // Invalid Role (out of range)
         public void UserWorkspaceCreateDto_ValidationTests(
