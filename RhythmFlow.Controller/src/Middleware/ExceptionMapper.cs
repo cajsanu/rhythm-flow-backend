4097740bad98adbfc98cf3ef3d5b6e91ed929619
Cajsa Nummelin <cajsa.nummelin@gmail.com>
2024-11-12 21:17:05 +0200

    add more possible exceptions to ExceptionMapper

--- RhythmFlow.Controller/src/Middleware/ExceptionMapper.cs
+++ RhythmFlow.Controller/src/Middleware/ExceptionMapper.cs
@@ -8,7 +8,18 @@ namespace RhythmFlow.Controller.src.Middleware
         private static readonly Dictionary<Type, (int StatusCode, string Message)> ExceptionMappings = new()
     {
         { typeof(InvalidOperationException), ((int)HttpStatusCode.BadRequest, "The operation is not valid.") },
-        { typeof(KeyNotFoundException), ((int)HttpStatusCode.NotFound, "The requested resource was not found.") }
+        { typeof(KeyNotFoundException), ((int)HttpStatusCode.NotFound, "The requested resource was not found.") },
+        { typeof(InvalidDataException), ((int)HttpStatusCode.BadRequest, "The data provided is invalid.") },
+        { typeof(UnauthorizedAccessException), ((int)HttpStatusCode.Unauthorized, "You are not authorized to access this resource.") },
+        { typeof(NotImplementedException), ((int)HttpStatusCode.NotImplemented, "This feature is not yet implemented.") },
+        { typeof(ArgumentException), ((int)HttpStatusCode.BadRequest, "The argument provided is invalid.") },
+        { typeof(FormatException), ((int)HttpStatusCode.BadRequest, "The format of the data provided is invalid.") },
+        { typeof(NotSupportedException), ((int)HttpStatusCode.NotImplemented, "This operation is not supported.") },
+        { typeof(OutOfMemoryException), ((int)HttpStatusCode.InternalServerError, "The system has run out of memory.") },
+        { typeof(IndexOutOfRangeException), ((int)HttpStatusCode.BadRequest, "The index is out of range.") },
+        { typeof(NullReferenceException), ((int)HttpStatusCode.BadRequest, "The object is null.") },
+        { typeof(ArgumentNullException), ((int)HttpStatusCode.BadRequest, "The argument provided is null.") },
+        { typeof(ArgumentOutOfRangeException), ((int)HttpStatusCode.BadRequest, "The argument is out of range.") }
     };
 
         public static (int StatusCode, string Message) MapException(Exception exception)
