7715c088b9deada3e9e020ba5de772fd4c6f5d91
Cajsa Nummelin <cajsa.nummelin@gmail.com>
2024-11-14 17:11:22 +0200

    WIP

--- RhythmFlow.Framework/src/Services/AuthenticationService.cs
+++ RhythmFlow.Framework/src/Services/AuthenticationService.cs
@@ -1,7 +1,14 @@
+using System.IdentityModel.Tokens.Jwt;
 using RhythmFlow.Application.src.ServiceInterfaces;
 using RhythmFlow.Domain.src.Entities;
 using RhythmFlow.Domain.src.RepoInterfaces;
 using RhythmFlow.Domain.src.ValueObjects;
+
+// Basically, there is no role connected to the user directly, but a user has a role connected to a workspace through the UserWorkspace join table.
+// When a user logs in, the authentication service will check if the user exists and if the password is correct. Then a JWT token will be generated and returned.
+// The JWT token will be used to authenticate the user in the future requests. However, the role of the user is not checked in the authentication process.
+// The role of the user should be checked in the authorization process when the user tries perform an action on a entity in the workspace.
+// This also means, for tickets and projects, we first have to check if the user has a role in the workspace before we can check if the user has the right role to perform the action.
 
 namespace RhythmFlow.Framework.src.Services
 {
@@ -23,12 +30,14 @@ namespace RhythmFlow.Framework.src.Services
             {
                 throw new UnauthorizedAccessException("Invalid email or password");
             }
+
             return GenerateJwtToken(user);
         }
 
-        private string GenerateJwtToken(User user)
+        private static string GenerateJwtToken(User user)
         {
-            throw new NotImplementedException();
+            var tokenHandler = new JwtSecurityTokenHandler();
+            return "JWT token";
         }
     }
 }
