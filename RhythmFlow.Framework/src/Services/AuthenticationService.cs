8d13ad6883889fb355e3db37ea30c1cbbaa05ace
Cajsa Nummelin <cajsa.nummelin@gmail.com>
2024-11-14 15:06:11 +0200

    implement AuthenticateUserAsync

--- RhythmFlow.Framework/src/Services/AuthenticationService.cs
+++ RhythmFlow.Framework/src/Services/AuthenticationService.cs
@@ -1,22 +1,34 @@
 using RhythmFlow.Application.src.ServiceInterfaces;
+using RhythmFlow.Domain.src.Entities;
 using RhythmFlow.Domain.src.RepoInterfaces;
+using RhythmFlow.Domain.src.ValueObjects;
 
 namespace RhythmFlow.Framework.src.Services
 {
     public class AuthenticationService : IAuthenticationService
     {
         private readonly IUserRepo _userRepo;
-        // private readonly IPasswordService _passwordService;
+        private readonly IPasswordService _passwordService;
 
         public AuthenticationService(IUserRepo userRepo)
         {
             _userRepo = userRepo;
-            // _passwordService = passwordService;
+            _passwordService = passwordService;
         }
 
-        public async Task<string?> AuthenticateUserAsync(string userEmail, string password)
+        public async Task<string?> AuthenticateUserAsync(Email userEmail, string password)
         {
-           throw new NotImplementedException();
+            var user = await _userRepo.GetUserByEmailAsync(userEmail);
+            if (user = null || !_passwordService.VerifyPassword(password, user.PasswordHash))
+            {
+                throw new UnauthorizedAccessException("Invalid email or password");
+            }
+            return GenerateJwtToken(user);
+        }
+
+        private string GenerateJwtToken(User user)
+        {
+            throw new NotImplementedException();
         }
     }
 }
