7af24353bdeb91f5a07365fa1e4e3ddb7b1c2880
Cajsa Nummelin <cajsa.nummelin@gmail.com>
2024-11-14 18:15:15 +0200

    implement GenerateJwtToken

--- RhythmFlow.Application/src/ServiceInterfaces/IAuthenticationService.cs
+++ RhythmFlow.Application/src/ServiceInterfaces/IAuthenticationService.cs
@@ -1,8 +1,10 @@
+using RhythmFlow.Domain.src.ValueObjects;
+
 namespace RhythmFlow.Application.src.ServiceInterfaces
 {
     public interface IAuthenticationService
     {
         // Authenticate user and return JWT token the authentication is successful
-        Task<string?> AuthenticateUserAsync(string userEmail, string password);
+        Task<string?> AuthenticateUserAsync(Email userEmail, string password);
     }
 }
--- RhythmFlow.Framework/src/Services/AuthenticationService.cs
+++ RhythmFlow.Framework/src/Services/AuthenticationService.cs
@@ -1,4 +1,7 @@
 using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
 using RhythmFlow.Application.src.ServiceInterfaces;
 using RhythmFlow.Domain.src.Entities;
 using RhythmFlow.Domain.src.RepoInterfaces;
@@ -9,24 +12,25 @@ using RhythmFlow.Domain.src.ValueObjects;
 // The JWT token will be used to authenticate the user in the future requests. However, the role of the user is not checked in the authentication process.
 // The role of the user should be checked in the authorization process when the user tries perform an action on a entity in the workspace.
 // This also means, for tickets and projects, we first have to check if the user has a role in the workspace before we can check if the user has the right role to perform the action.
-
 namespace RhythmFlow.Framework.src.Services
 {
     public class AuthenticationService : IAuthenticationService
     {
         private readonly IUserRepo _userRepo;
         private readonly IPasswordService _passwordService;
+        private readonly IConfiguration _configuration;
 
-        public AuthenticationService(IUserRepo userRepo)
+        public AuthenticationService(IUserRepo userRepo, IPasswordService passwordService, IConfiguration configuration)
         {
             _userRepo = userRepo;
             _passwordService = passwordService;
+            _configuration = configuration;
         }
 
         public async Task<string?> AuthenticateUserAsync(Email userEmail, string password)
         {
             var user = await _userRepo.GetUserByEmailAsync(userEmail);
-            if (user = null || !_passwordService.VerifyPassword(password, user.PasswordHash))
+            if (user == null || !_passwordService.VerifyPassword(password, user.PasswordHash))
             {
                 throw new UnauthorizedAccessException("Invalid email or password");
             }
@@ -34,10 +38,35 @@ namespace RhythmFlow.Framework.src.Services
             return GenerateJwtToken(user);
         }
 
-        private static string GenerateJwtToken(User user)
+        private string GenerateJwtToken(User user)
         {
+            // Create key for the token
+            var key = Encoding.ASCII.GetBytes(_configuration["Jwt:Secret"]);
+            if (key.Length < 16)
+            {
+                throw new InvalidOperationException("JWT secret must be at least 16 characters long");
+            }
+
+            // Add claims for the token
+            var claims = new List<Claim>
+            {
+                new (ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new (ClaimTypes.Email, user.Email.Value.ToString())
+            };
+
             var tokenHandler = new JwtSecurityTokenHandler();
-            return "JWT token";
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddMinutes(Convert.ToDouble(_configuration["Jwt:TokenLifetime"])),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
+                Issuer = _configuration["Jwt:Issuer"],
+                Audience = _configuration["Jwt:Audience"]
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
         }
     }
 }
