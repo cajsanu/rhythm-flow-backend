b1c037824406d01b6f93bb7fbad0a3c5834986b9
Cajsa Nummelin <cajsa.nummelin@gmail.com>
2024-11-14 11:16:21 +0200

    implement HashPassword

--- RhythmFlow.Framework/src/Services/PasswordService.cs
+++ RhythmFlow.Framework/src/Services/PasswordService.cs
@@ -1,10 +1,16 @@
+using RhythmFlow.Application.src.ServiceInterfaces;
+
 namespace RhythmFlow.Framework.src.Services
 {
     public class PasswordService : IPasswordService
     {
+        // Hash the password before storing it in the database
         public string HashPassword(string password)
         {
-            throw new NotImplementedException();
+            // Hash password using BCrypt, which automatically salts the password
+            // and has a default work factor of 11
+            var passwordHash = BCrypt.Net.BCrypt.HashPassword(password);
+            return passwordHash;
         }
 
         public bool VerifyPassword(string password, string passwordHash)
