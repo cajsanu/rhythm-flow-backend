98db787cffc7ac6f56f9e4ccbe5e60eacfd6cdbd
Son Bui <ngocson.bui0312@gmail.com>
2024-11-21 11:18:46 +0200

    seeding data and build the model for database using EF Core

--- RhythmFlow.Framework/src/Data/DatabaseContext.cs
+++ RhythmFlow.Framework/src/Data/DatabaseContext.cs
@@ -1,92 +1,59 @@
+using Microsoft.EntityFrameworkCore;
 using RhythmFlow.Domain.src.Entities;
 using RhythmFlow.Domain.src.ValueObjects;
 
 namespace RhythmFlow.Framework.src.Data
 {
-    public class AppDbContext
+    public class AppDbContext(DbContextOptions<AppDbContext> dbContextOptions, IConfiguration configuration) : DbContext(dbContextOptions)
     {
         // In-memory storage for each entity type
-        public List<User> Users { get; set; } = [];
-        public List<Project> Projects { get; set; } = [];
-        public List<Ticket> Tickets { get; set; } = [];
-        public List<Workspace> Workspaces { get; set; } = [];
-        public List<UserWorkspace> UserWorkspaces { get; set; } = [];
+        public DbSet<User> Users { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<Workspace> Workspaces { get; set; }
+        public DbSet<UserWorkspace> UserWorkspaces { get; set; }
 
-        private static readonly object _lock = new ();
+        private readonly IConfiguration _configuration = configuration;
 
-        public static AppDbContext Instance
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            get
+            optionsBuilder
+            .UseNpgsql(_configuration.GetConnectionString("DefaultConnection"))
+            .EnableDetailedErrors() // not in production
+            .EnableSensitiveDataLogging() // not in production
+            .UseSnakeCaseNamingConvention();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
             {
-                lock (_lock)
+                entity.OwnsOne(user => user.Email, email =>
                 {
-                    _instance ??= new AppDbContext();
-                    return _instance;
-                }
-            }
-        }
+                    email.Property(s => s.Value).HasColumnName("email").IsRequired();
+                });
+            });
+            modelBuilder.Entity<User>().HasIndex(u => u.Email).IsUnique();
+            modelBuilder.Entity<UserWorkspace>().HasKey(uw => new { uw.UserId, uw.WorkspaceId });
 
-        private static AppDbContext? _instance;
+            // convert Project enum type to string
+            modelBuilder.Entity<Project>()
+                .Property(e => e.Status)
+                .HasConversion(
+                    v => v.ToString(),
+                    v => (Status)Enum.Parse(typeof(Status), v));
 
-        public AppDbContext()
-        {
-            SeedData(); // Initialize with some dummy data
-        }
-
-        private void SeedData()
-        {
-            // Generate Users
-            var user1 = new User("John", "Doe", "john.doe@example.com", "passwordHash");
-            var user2 = new User("Jane", "Smith", "jane.smith@example.com", "passwordHash123");
-            var user3 = new User("Michael", "Johnson", "michael.johnson@example.com", "securePass456");
-            var user4 = new User("Emily", "Davis", "emily.davis@example.com", "hashedPassword789");
-            var user5 = new User("Chris", "Brown", "chris.brown@example.com", "passHash321");
-            var user6 = new User("Sophia", "Wilson", "sophia.wilson@example.com", "encryptedPass654");
-            var user7 = new User("Daniel", "Martinez", "daniel.martinez@example.com", "hashedPass987");
-            var user8 = new User("Olivia", "Garcia", "olivia.garcia@example.com", "passwordHash159");
-            var user9 = new User("Matthew", "Anderson", "matthew.anderson@example.com", "hashPass753");
-            Users.AddRange([user1, user2, user3, user4, user5, user6, user7, user8, user9]);
-
-            // Generate Workspace
-            var workspace1 = new Workspace("Marketing", user1.Id);
-            var workspace2 = new Workspace("Development", user2.Id);
-            var workspace3 = new Workspace("Design", user3.Id);
-            var workspace4 = new Workspace("Sales", user4.Id);
-            var workspace5 = new Workspace("HR", user5.Id);
-            Workspaces.AddRange([workspace1, workspace2, workspace3, workspace4, workspace5]);
-
-            // Generate Project
-            var project1 = new Project("Alpha", "Conquer the Galaxy", DateTime.Now.AddDays(2), DateTime.Now.AddDays(50), Status.InProgress, workspace1.Id);
-            var project2 = new Project("Beta", "Develop AI Assistant", DateTime.Now.AddDays(5), DateTime.Now.AddDays(60), Status.NotStarted, workspace2.Id);
-            var project3 = new Project("Gamma", "Revamp Website Design", DateTime.Now.AddDays(3), DateTime.Now.AddDays(45), Status.InProgress, workspace3.Id);
-            var project4 = new Project("Delta", "Launch Mobile App", DateTime.Now.AddDays(7), DateTime.Now.AddDays(90), Status.InProgress, workspace4.Id);
-            var project5 = new Project("Epsilon", "Implement Cloud Migration", DateTime.Now.AddDays(1), DateTime.Now.AddDays(120), Status.Cancelled, workspace5.Id);
-            var project6 = new Project("Zeta", "Optimize Data Pipeline", DateTime.Now.AddDays(10), DateTime.Now.AddDays(80), Status.InProgress, workspace5.Id);
-            Projects.AddRange([project1, project2, project3, project4, project5, project6]);
-
-            // Generate tickets
-            var ticket1 = new Ticket("Create Database", "using Postgres", Priority.High, DateTime.Now.AddDays(7), Status.InProgress, project1.Id, TicketType.Bug);
-            var ticket2 = new Ticket("Fix Login Issue", "Resolve authentication error", Priority.High, DateTime.Now.AddDays(3), Status.InProgress, project1.Id, TicketType.Bug);
-            var ticket3 = new Ticket("Design Landing Page", "Create wireframe for new landing page", Priority.Medium, DateTime.Now.AddDays(14), Status.Cancelled, project1.Id, TicketType.Feature);
-            var ticket4 = new Ticket("Update Privacy Policy", "Review and update policy document", Priority.Low, DateTime.Now.AddDays(30), Status.NotStarted, project2.Id, TicketType.TechnicalDebt);
-            var ticket5 = new Ticket("Optimize API", "Enhance performance of existing API calls", Priority.High, DateTime.Now.AddDays(10), Status.InProgress, project2.Id, TicketType.Feature);
-            var ticket6 = new Ticket("Schedule Training", "Plan onboarding session for new hires", Priority.Medium, DateTime.Now.AddDays(7), Status.NotStarted, project2.Id, TicketType.TechnicalDebt);
-            var ticket7 = new Ticket("Add Dark Mode", "Implement dark mode toggle for users", Priority.High, DateTime.Now.AddDays(21), Status.Cancelled, project5.Id, TicketType.Bug);
-            var ticket8 = new Ticket("Bug in Report Generation", "Fix issue with incorrect data rendering", Priority.High, DateTime.Now.AddDays(5), Status.InProgress, project6.Id, TicketType.Feature);
-            Tickets.AddRange([ticket1, ticket2, ticket3, ticket4, ticket5, ticket6, ticket7, ticket8]);
-
-            // Generate user workspace
-            // Project manager
-            var userWorkspace1 = new UserWorkspace(user1.Id, workspace1.Id, Role.ProjectManager);
-            var userWorkspace2 = new UserWorkspace(user2.Id, workspace2.Id, Role.ProjectManager);
-            var userWorkspace3 = new UserWorkspace(user3.Id, workspace3.Id, Role.ProjectManager);
-
-            // developer
-            var userWorkspace4 = new UserWorkspace(user4.Id, workspace1.Id, Role.ProjectManager);
-            var userWorkspace5 = new UserWorkspace(user5.Id, workspace1.Id, Role.ProjectManager);
-            var userWorkspace6 = new UserWorkspace(user6.Id, workspace2.Id, Role.Developer);
-            var userWorkspace7 = new UserWorkspace(user7.Id, workspace2.Id, Role.Developer);
-            UserWorkspaces.AddRange([userWorkspace1, userWorkspace2, userWorkspace3, userWorkspace4, userWorkspace5, userWorkspace6, userWorkspace7]);
+            // convert Ticket enum type to tring
+            modelBuilder.Entity<Ticket>()
+                .Property(e => e.Status)
+                .HasConversion(
+                    v => v.ToString(),
+                    v => (Status)Enum.Parse(typeof(Status), v)); // convert enum type to string
+            modelBuilder.Entity<Ticket>()
+                .Property(e => e.Priority)
+                .HasConversion(
+                    v => v.ToString(),
+                    v => (Priority)Enum.Parse(typeof(Priority), v)); // convert enum type to string
         }
 
         // CRUD Simulation Methods
@@ -131,11 +98,11 @@ namespace RhythmFlow.Framework.src.Data
             }
         }
 
+#pragma warning disable CS8603 // Possible null reference return.
         private List<T> GetDbSet<T>()
             where T : BaseEntity
         {
             // cannot use switch here
-#pragma warning disable CS8603 // Possible null reference return.
             if (typeof(T) == typeof(User))
                 return Users as List<T>;
 
@@ -147,8 +114,9 @@ namespace RhythmFlow.Framework.src.Data
                 return Workspaces as List<T>;
             if (typeof(T) == typeof(UserWorkspace))
                 return UserWorkspaces as List<T>;
-#pragma warning restore CS8603 // Possible null reference return.
+
             throw new InvalidOperationException("Invalid entity type");
         }
+#pragma warning restore CS8603 // Possible null reference return.
     }
 }
--- RhythmFlow.Framework/src/Data/SeededData.cs
+++ RhythmFlow.Framework/src/Data/SeededData.cs
@@ -16,7 +16,6 @@ namespace RhythmFlow.Framework.src.Data
         private static readonly User User7 = new ("Daniel", "Martinez", "daniel.martinez@example.com", "hashedPass987");
         private static readonly User User8 = new ("Olivia", "Garcia", "olivia.garcia@example.com", "passwordHash159");
         private static readonly User User9 = new ("Matthew", "Anderson", "matthew.anderson@example.com", "hashPass753");
-        // Users.AddRange([User1, User2, User3, User4, User5, User6, User7, User8, User9]);
 
         // Generate Workspace
         private static readonly Workspace Workspace1 = new ("Marketing", User1.Id);
@@ -24,7 +23,6 @@ namespace RhythmFlow.Framework.src.Data
         private static readonly Workspace Workspace3 = new ("Design", User3.Id);
         private static readonly Workspace Workspace4 = new ("Sales", User4.Id);
         private static readonly Workspace Workspace5 = new ("HR", User5.Id);
-        // Workspaces.AddRange([workspace1, workspace2, workspace3, workspace4, workspace5]);
 
         // Generate Project
         private static readonly Project Project1 = new ("Alpha", "Conquer the Galaxy", DateTime.Now.AddDays(2), DateTime.Now.AddDays(50), Status.InProgress, Workspace1.Id);
@@ -33,7 +31,6 @@ namespace RhythmFlow.Framework.src.Data
         private static readonly Project Project4 = new ("Delta", "Launch Mobile App", DateTime.Now.AddDays(7), DateTime.Now.AddDays(90), Status.InProgress, Workspace4.Id);
         private static readonly Project Project5 = new ("Epsilon", "Implement Cloud Migration", DateTime.Now.AddDays(1), DateTime.Now.AddDays(120), Status.Cancelled, Workspace5.Id);
         private static readonly Project Project6 = new ("Zeta", "Optimize Data Pipeline", DateTime.Now.AddDays(10), DateTime.Now.AddDays(80), Status.InProgress, Workspace5.Id);
-        // Projects.AddRange([project1, project2, project3, project4, project5, project6]);
 
         // Generate tickets
         private static readonly Ticket Ticket1 = new ("Create Database", "using Postgres", Priority.High, DateTime.Now.AddDays(7), Status.InProgress, Project1.Id, TicketType.Bug);
@@ -44,7 +41,6 @@ namespace RhythmFlow.Framework.src.Data
         private static readonly Ticket Ticket6 = new ("Schedule Training", "Plan onboarding session for new hires", Priority.Medium, DateTime.Now.AddDays(7), Status.NotStarted, Project2.Id, TicketType.TechnicalDebt);
         private static readonly Ticket Ticket7 = new ("Add Dark Mode", "Implement dark mode toggle for Users", Priority.High, DateTime.Now.AddDays(21), Status.Cancelled, Project5.Id, TicketType.Bug);
         private static readonly Ticket Ticket8 = new ("Bug in Report Generation", "Fix issue with incorrect data rendering", Priority.High, DateTime.Now.AddDays(5), Status.InProgress, Project6.Id, TicketType.Feature);
-        // Tickets.AddRange([ticket1, ticket2, ticket3, ticket4, ticket5, ticket6, ticket7, ticket8]);
 
         // Generate User workspace
         // Project manager
@@ -57,7 +53,6 @@ namespace RhythmFlow.Framework.src.Data
         private static readonly UserWorkspace UserWorkspace5 = new (User1.Id, Workspace1.Id, Role.ProjectManager);
         private static readonly UserWorkspace UserWorkspace6 = new (User6.Id, Workspace2.Id, Role.Developer);
         private static readonly UserWorkspace UserWorkspace7 = new (User7.Id, Workspace2.Id, Role.Developer);
-        // UserWorkspaces.AddRange([UserWorkspace1, UserWorkspace2, UserWorkspace3, UserWorkspace4, UserWorkspace5, UserWorkspace6, UserWorkspace7]);
 
         public static void Seed(ModelBuilder modelBuilder)
         {
