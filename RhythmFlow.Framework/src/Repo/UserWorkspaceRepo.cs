7e5debfde562c78e789ceadd87f6cee3a691231b
Anouar Belila <anouarbelila@Anouars-MacBook-Pro.local>
2024-11-23 15:48:53 +0200

    Add repo methods

--- RhythmFlow.Framework/src/Repo/UserWorkspaceRepo.cs
+++ RhythmFlow.Framework/src/Repo/UserWorkspaceRepo.cs
@@ -1,3 +1,5 @@
+using System.Data.Common;
+using Microsoft.EntityFrameworkCore;
 using RhythmFlow.Domain.src.Entities;
 using RhythmFlow.Domain.src.RepoInterfaces;
 using RhythmFlow.Domain.src.ValueObjects;
@@ -8,15 +10,20 @@ namespace RhythmFlow.Framework.src.Repo
     public class UserWorkspaceRepo(AppDbContext context) : IUserWorkspaceRepo
     {
         private readonly AppDbContext _context = context;
+        private readonly DbSet<UserWorkspace> _userWorkspaces = context.Set<UserWorkspace>();
 
-        public Task<UserWorkspace?> AssignRoleToUserInWorkspace(Guid userId, Guid workspaceId, Role role)
+        public async Task<UserWorkspace?> AssignRoleToUserInWorkspace(Guid userId, Guid workspaceId, Role role)
         {
-            throw new NotImplementedException();
+            UserWorkspace userWorkspace = new UserWorkspace(userId, workspaceId, role);
+            var added = await _userWorkspaces.AddAsync(userWorkspace);
+            await _context.SaveChangesAsync();
+            return added.Entity;
         }
 
         public Task<IEnumerable<Workspace>> GetAllUserWorkspacesByUserIdAsync(Guid userId)
         {
-            throw new NotImplementedException();
+            var workspaces = _context.GetWorkspacesJoinedByUserId(userId);
+            return Task.FromResult(workspaces);
         }
 
         public Task<UserWorkspace?> GetUserWorkspaceByUserIdAndWorkspaceIdAsync(Guid userId, Guid workspaceId)
@@ -27,7 +34,8 @@ namespace RhythmFlow.Framework.src.Repo
 
         public Task<IEnumerable<Workspace>> GetWorkspacesOwnedByUserAsync(Guid workspaceId)
         {
-            throw new NotImplementedException();
+            var workspaces = _context.GetWorkspacesOwnedByUser(workspaceId);
+            return Task.FromResult(workspaces);
         }
     }
 }
