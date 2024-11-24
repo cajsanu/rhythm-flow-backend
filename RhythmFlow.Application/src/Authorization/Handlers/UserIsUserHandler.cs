4f5b87171f4e97637e0e506242ad69e6a6d67826
Son Bui <ngocson.bui0312@gmail.com>
2024-11-24 10:18:14 +0200

    added check for user is user for CRUD operation on user

---
+++ RhythmFlow.Application/src/Authorization/Handlers/UserIsUserHandler.cs
@@ -0,0 +1,53 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using RhythmFlow.Application.src.ServiceInterfaces;
+
+namespace RhythmFlow.Application.src.Authorization.Handlers
+{
+    public class UserIsUserHandler(IUserService userService, IHttpContextAccessor httpContextAccessor) : AuthorizationHandler<UserInProjectRequirement>
+    {
+        private readonly IUserService _userService = userService;
+        private readonly IHttpContextAccessor _httpContextAccessor = httpContextAccessor;
+
+        protected override async Task HandleRequirementAsync(
+            AuthorizationHandlerContext context,
+            UserInProjectRequirement requirement)
+        {
+            // Extract target user ID from route data
+            var routeData = _httpContextAccessor.HttpContext?.GetRouteData();
+            if (routeData == null || !routeData.Values.TryGetValue("userId", out var targetUserIdValue)
+                || !Guid.TryParse(targetUserIdValue?.ToString(), out var targetUserId))
+            {
+                context.Fail();
+                return;
+            }
+
+            // Get user ID from the claims
+            if (!Guid.TryParse(context.User.FindFirst(ClaimTypes.NameIdentifier)?.Value, out var userId))
+            {
+                context.Fail();
+                return;
+            }
+
+            // validate for user Existence
+            var user = await _userService.GetByIdAsync(userId);
+            if (user == null)
+            {
+                context.Fail();
+                return;
+            }
+
+            // Validate if the user is the same as target user
+            if (targetUserId != userId)
+            {
+                context.Fail();
+            }
+            else
+            {
+                context.Succeed(requirement);
+            }
+        }
+    }
+}
