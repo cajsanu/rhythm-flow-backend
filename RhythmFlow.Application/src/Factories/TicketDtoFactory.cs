e81b594aa9f01deafe5f64b07de3eaaf352faef1
Cajsa Nummelin <cajsa.nummelin@gmail.com>
2024-11-26 11:36:14 +0200

    include users in ticket read dto

--- RhythmFlow.Application/src/Factories/TicketDtoFactory.cs
+++ RhythmFlow.Application/src/Factories/TicketDtoFactory.cs
@@ -1,4 +1,5 @@
 using RhythmFlow.Application.src.DTOs.Tickets;
+using RhythmFlow.Application.src.DTOs.Users;
 using RhythmFlow.Application.src.FactoryInterfaces;
 using RhythmFlow.Domain.src.Entities;
 
@@ -29,6 +30,13 @@ namespace RhythmFlow.Application.src.Factories
                 Priority = entity.Priority,
                 Type = entity.Type,
                 ProjectId = entity.ProjectId,
+                Users = entity.Users.Select(u => new UserReadDto
+                {
+                    Id = u.Id,
+                    FirstName = u.FirstName,
+                    LastName = u.LastName,
+                    Email = u.Email.Value
+                }).ToList()
             };
         }
 
