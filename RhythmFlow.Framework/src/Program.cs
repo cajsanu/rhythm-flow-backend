6becc427229c4bb08f41980745b5e8f5d5ebd470
Son Bui <ngocson.bui0312@gmail.com>
2024-11-27 23:40:09 +0200

    update swagger so it allows the use of Token for request

--- RhythmFlow.Framework/src/Program.cs
+++ RhythmFlow.Framework/src/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.ApplicationModels;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using RhythmFlow.Application.src.Authorization;
 using RhythmFlow.Application.src.Authorization.Handlers;
 using RhythmFlow.Application.src.DTOs.Projects;
@@ -28,7 +29,34 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(opt =>
+{
+    opt.SwaggerDoc("v1", new OpenApiInfo { Title = "MyAPI", Version = "v1" });
+    opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "bearer"
+    });
+
+    opt.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 
 // Configure lowercase URLs
 builder.Services.AddRouting(options => options.LowercaseUrls = true);
@@ -131,12 +159,7 @@ builder.Services.AddTransient<ExceptionHandlerMiddleware>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    // app.UseSwagger();
-    // app.UseSwaggerUI();
-}
-else
+if (!app.Environment.IsDevelopment())
 {
     // putting this here since this can hide errors during development
     app.UseMiddleware<ExceptionHandlerMiddleware>();
