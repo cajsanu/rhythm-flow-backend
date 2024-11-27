69c91151842654a1e2839179cc37e6fc03112e17
Cajsa Nummelin <cajsa.nummelin@gmail.com>
2024-11-27 16:23:31 +0200

    fix project update

--- RhythmFlow.Application/src/DTOs/Projects/ProjectUpdateDto.cs
+++ RhythmFlow.Application/src/DTOs/Projects/ProjectUpdateDto.cs
@@ -12,7 +12,7 @@ namespace RhythmFlow.Application.src.DTOs.Projects
         public DateOnly EndDate { get; set; }
         public Status Status { get; set; }
         public Guid WorkspaceId { get; set; }
-        public ICollection<Guid> UsersId { get; set; } = [];
+        // public ICollection<Guid> UserIds { get; set; } = [];
 
         public IBaseUpdateDto<Project> ToDto(Project entity)
         {
